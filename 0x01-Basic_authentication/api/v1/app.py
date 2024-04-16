460db03997c02f507a51dd7964cfbf0cd0df0608
swiftleah <leahrosehenry@gmail.com>
2024-04-16 09:10:46 +0000

    Task 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' basic cofe for Flask '''
+''' basic code for Flask '''
 from flask import Flask, jsonify
 from api.v1.views.index import app_views
 
@@ -11,8 +11,16 @@ app.register_blueprint(app_views)
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     ''' handles error 401 for unauthorized req '''
-    response = jsonify({'error': 'Unauthorized'})
+    response = jsonify({"error": "Unauthorized"})
     response.status_code = 401
+    return response
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    ''' handles error 403 - no access '''
+    response = jsonify({"error": "Forbidden"})
+    response.status_code = 403
     return response
 
 
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -6,8 +6,15 @@ from flask import abort, Blueprint
 app_views = Blueprint('app_views', __name__)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
     ''' defines route /api/v1/unauthorized
     when GET req is made -> returns 401 error '''
     abort(401)
+
+
+@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    ''' GET req /api/v1/forbidden
+    returns forbidden error '''
+    abort(403)
