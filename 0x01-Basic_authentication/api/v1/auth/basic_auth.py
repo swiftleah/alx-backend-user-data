5e8aeef78c59552ccb3375e81b9bca807d3ce250
swiftleah <leahrosehenry@gmail.com>
2024-04-16 10:48:49 +0000

    Task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,19 @@ class BasicAuth(Auth):
             return decoded_str
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        ''' returns user email and password from base64
+        decoded value '''
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(
+                ':',
+                1)
+        return email, password
