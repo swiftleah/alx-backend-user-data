dbffd69089d47740488e5172699f901f689710cb
swiftleah <leahrosehenry@gmail.com>
2024-04-16 09:38:29 +0000

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
     ''' manages API authentication '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' returns false '''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' returns None '''
