09c5a7fd4df34c26dd12ddfa97b7b6c8c47af402
swiftleah <leahrosehenry@gmail.com>
2024-04-16 10:44:50 +0000

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' basic authentication '''
 from api.v1.auth.auth import Auth
+import binascii
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +22,18 @@ class BasicAuth(Auth):
             return None
         base64_part = parts[1]
         return base64_part
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        ''' returns decoded value of Base64 string '''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except (binascii.Error, UnicodeDecodeError):
+            return None
