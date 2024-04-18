7d9b93f65b8e974849bf5e770b680610f1225d32
Leah <125441128+swiftleah@users.noreply.github.com>
2024-04-18 16:57:26 +0200

    Update index.py

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
-''' basic code for Flask application '''
-from flask import abort, Blueprint, jsonify
+""" Module of Index views"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
 
-app_views = Blueprint('app_views', __name__)
-
-
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status() -> str:
-    ''' returns OK '''
+    """Return the status of the API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> None:
-    ''' defines route /api/v1/unauthorized
-    when GET req is made -> returns 401 error '''
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """Return the number of each objects"""
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route("/unauthorized/", strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized"""
     abort(401)
 
 
-@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> None:
-    ''' GET req /api/v1/forbidden
-    returns forbidden error '''
+@app_views.route("/forbidden/", strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden"""
     abort(403)
