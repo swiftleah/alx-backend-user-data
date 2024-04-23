50690e68566a1cad66baca305159c2e68b9673ff
swiftleah <leahrosehenry@gmail.com>
2024-04-23 08:50:06 +0000

    Task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,3 +50,13 @@ class DB:
             raise
         except NoResultFound:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        ''' updates user attributes '''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
