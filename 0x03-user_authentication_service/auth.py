3d540235e169e0a23d6b88d745bf54ad05fe05de
swiftleah <leahrosehenry@gmail.com>
2024-04-24 15:18:27 +0000

    Task 7 & 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,12 +4,17 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     ''' returns hashed password '''
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def _generate_uuid() -> str:
+    """Generate a UUID and return its string representation"""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -27,3 +32,11 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if login credentials are valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
